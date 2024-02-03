e11da9587b1904afe2945294990fd89f3bc6662a
yelly <kk3p1q1@gmail.com>
2024-02-03 10:26:01 +0900

    각도, 휘어짐, 명도 등의 다양성 추가

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// const 로 따로 선언된 변수들은 추후에 입력 값으로 받을 것.
 class Root {
   constructor(x, y) {
     this.x = x;
@@ -18,17 +19,29 @@ class Root {
     this.maxSize = Math.random() * (finalMaxSize - finalMinSize) + finalMinSize; // 이 값이 클수록 완전히 퍼지는데 걸리는 시간은 오래 걸린다.
     this.size = Math.random() * 1 + 2;
 
-    this.angle = Math.random() * 6.2;
+    this.velocitySize = Math.random() * 0.2 + 0.05; // 최대 사이즈에 도달할 때까지 랜덤한 속도로 퍼지기 위함
+    this.velocityAngleX = Math.random() * 0.6 - 0.3;
+    this.angleX = Math.random() * 6.2;
+    this.velocityAngleY = Math.random() * 0.6 - 0.3;
+    this.angleY = Math.random() * 6.2;
+
+    this.lightness = 10;
   }
   update() {
-    this.x += this.speedX + Math.sin(this.angle);
-    this.y += this.speedY + Math.sin(this.angle);
-    this.size += 0.1;
-    this.angle += 0.1;
+    this.x += this.speedX + Math.sin(this.angleX);
+    this.y += this.speedY + Math.sin(this.angleY);
+    this.size += this.velocitySize;
+    this.angleX += this.velocityAngleX;
+    this.angleY += this.velocityAngleY;
+
+    if (this.lightness < 90) {
+      this.lightness += 1;
+    }
+
     if (this.size < this.maxSize) {
       ctx.beginPath(); // 새로운 경로 시작
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2); // 원 그리기. (x, y)는 중심 좌표, size는 반지름
-      ctx.fillStyle = "hsl(140,100%,50%)"; // 색상, 채도, 명도
+      ctx.fillStyle = `hsl(140,100%,${this.lightness}%)`; // 색상, 채도, 명도
       ctx.fill(); // 설정된 색상으로 원의 내부를 채우기
       ctx.stroke(); // 원의 테두리
       requestAnimationFrame(() => this.update());
